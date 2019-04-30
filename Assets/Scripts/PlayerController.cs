5a4d7d329fd93bb331f599831627b16dcf3c2d15
ramoomar99 <rtaha4@gmail.com>
2019-04-30 00:57:18 -0500

    final extra credit mods

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,16 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Text countText;
     public Text winText;
+    public Text timeText;
+    public Text speedText;
+    public Button resetButton;
+    public GameObject pickUpObject;
 
     private Rigidbody rb;
     private int count;
+    private float timeCounter;
+    private bool gameOver;
+    private float speedMag;
 
 
     private void Start()
@@ -19,6 +26,19 @@ public class PlayerController : MonoBehaviour
         count = 0;
         setCountText();
         winText.text = "";
+        timeCounter = 0;
+        gameOver = false;
+        speedMag = 0;
+        updateSpeed();
+    }
+
+    private void Update()
+    {
+        if (!gameOver)
+        {
+            timeCounter += Time.deltaTime;
+            setTimeText();
+        }
     }
 
     private void FixedUpdate()
@@ -29,6 +49,7 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        updateSpeed();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,16 +60,49 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
             //increment Count & String
             count = count + 1;
+            if(count >= 12)
+            {
+                gameOver = true;
+            }
             setCountText();
+            Debug.Log("Collided with square");
+        }
+        else if(other.gameObject.CompareTag("Fast Patch"))
+        {
+            Debug.Log("Collided with fast patch");
+            var vel = rb.velocity;
+
+            rb.AddForce(vel * 5 * speed);
+            updateSpeed();
+        }
+        else if (other.gameObject.CompareTag("Bouncer"))
+        {
+            rb.velocity = rb.velocity * -1;
         }
     }
 
     void setCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(gameOver)
         {
             winText.text = "You Win!";
         }
+    }
+
+    void setTimeText()
+    {
+        timeText.text = "Time: " + timeCounter.ToString();
+    }
+
+    void updateSpeed()
+    {
+        var vel = rb.velocity;
+        var tmpSpeed = vel.magnitude;
+        if(tmpSpeed > speedMag)
+        {
+            speedMag = tmpSpeed;
+        }
+        speedText.text = "Top Speed: " + speedMag.ToString();
     }
 }
